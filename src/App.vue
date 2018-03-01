bc825ffd553d383e58812e615e4d9063dd1642fb
Machteld Vlietstra <>
2018-03-01 13:26:51 +0000

    Commit of initial project, setup of boilerplate files and folder structure

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+	.layout
+		p A New Vue Project
+</template>
+
+<script>export default {
+	name: 'App',
+	
+	data: function() {
+		return {
+			
+		}
+	},
+	
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+var path = require('path')
+var webpack = require('webpack')
+var resolve = require('resolve')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue',
+        options: {
+          // vue-loader options go here
+          //scss: 'style!css!sass'
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.s[a|c]ss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json', '.scss'],
+    alias: {
+      'vue$': 'vue/dist/vue.js',
+      '@': path.resolve('src'),
+      styles: path.resolve('src/assets/sass')
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
