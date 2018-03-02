79e2a9e1d7c37c068c9290e8790d76953ac488ac
Machteld Vlietstra <>
2018-03-02 12:01:07 +0000

    Add new profile item button added and styled

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template lang="pug">
 	.layout
 		app-header
+		app-add-item-button
 		app-item(:items="items" @itemDeleted="deleteItem")
 
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import AddItem from './components/AddItemButton.vue';
 import Item from './components/Item.vue';
 
 export default {
 	name: 'App',
 	components: {
 		appHeader: Header,
+		appAddItemButton: AddItem,
 		appItem: Item,
 	},
 	data: function() {
