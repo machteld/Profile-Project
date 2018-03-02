e39a1724f1f8473af31128fe6211feed06764ee8
Machteld Vlietstra <>
2018-03-02 12:26:44 +0000

    Added form in overlay and set toggle to add item button

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,14 @@
 		app-header
 		app-add-item-button
 		app-item(:items="items" @itemDeleted="deleteItem")
-
+		app-form(:items="items" @itemAdded="newItem")
 </template>
 
 <script>
 import Header from './components/Header.vue';
 import AddItem from './components/AddItemButton.vue';
 import Item from './components/Item.vue';
+import Form from './components/Form.vue';
 
 export default {
 	name: 'App',
@@ -17,36 +18,66 @@ export default {
 		appHeader: Header,
 		appAddItemButton: AddItem,
 		appItem: Item,
+		appForm: Form,
 	},
 	data: function() {
 		return {
 			items: [
 				{
-					name: 'machteld',
+					name: 'Machteld Vlietstra',
 					age: '41',
 					jobTitle: 'Frontend Developer',
-					selectedIcon: 'gitlab'
+					selectedIcon: 'github',
+					bio: 'Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before.'
 				},
 				{
 					name: 'Jan Klaassen',
-					age: '41',
-					jobTitle: 'Frontend Developer',
-					selectedIcon: 'gitlab'
+					age: '32',
+					jobTitle: 'Backend Developer',
+					selectedIcon: 'gitlab',
+					bio:'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia?'
 				},
 				{
 					name: 'Jan Janssen',
-					age: '41',
-					jobTitle: 'Frontend Developer',
-					selectedIcon: 'github'
+					age: '51',
+					jobTitle: 'Lead Frontend Developer',
+					selectedIcon: 'user',
+					bio: 'Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. '
 				},
 				{
 					name: 'Jan Franssen',
 					age: '25',
-					jobTitle: 'Frontend Developer',
-					selectedIcon: 'user'
+					jobTitle: 'Junior Frontend Developer',
+					selectedIcon: 'gitlab',
+					bio: 'Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat tbh semiotics artisan synth stumptown gastropub cornhole celiac swag.'
 				},
 			],
 			icons: ['github','gitlab', 'user'],
+			isActive: false
+		}
+	},
+	mounted() {
+		this.getItems();
+	},
+	methods: {
+		getItems() {
+			if (localStorage.getItem('item_list')) {
+				this.items = JSON.parse(localStorage.getItem('item_list'));
+			}
+		},
+		newItem(item) {
+			this.items.push(item);
+		},
+		deleteItem(index) {
+			this.items.splice(index, 1);
+		},		
+	},
+	watch: {
+		items: {
+			handler: function(updatedList) {
+				localStorage.setItem('item_list', JSON.stringify(updatedList));
+			},
+			deep: true
 		}
 	},
 	
---
+++ src/components/Form.vue
@@ -0,0 +1,90 @@
+<template lang="pug">
+	.overlay(v-bind:class="{ 'new-item__overlay -open': isActive }")
+		button(class="close" @click="closeOverlay")
+			svg(class="icon-x")
+				use(class="icon-x" xlink:href="src/assets/symbol-defs.svg#icon-x")
+		.overlay__body
+			form()
+				legend Voeg een nieuw profiel toe
+				.form-field--select
+					label(for="select-icon" class="label__select") Kies een icoon
+					select(id="select-icon" class="input__select" v-model="item.selectedIcon")
+						option(v-for="i in icons" :key="i") {{ i }}
+				.form-field
+					input(type="text" class="input-field" v-on:focus="focusFn" v-model="item.name")
+					label Naam
+				.form-field
+					input(type="text" class="input-field" v-on:focus="focusFn" v-model="item.jobTitle")
+					label Functietitel
+				.form-field
+					input(type="text" class="input-field" v-on:focus="focusFn" v-model="item.age")
+					label Leeftijd
+				.form-field
+					textarea(class="form-field" rows="3" class="input-field" v-on:focus="focusFn" v-model="item.bio")
+					label Bio
+
+				button(class="btn btn__submit" id="form-btn" type="submit" disabled @click.prevent="newItem") 
+					svg(class="icon-user-plus")
+						use(class="icon-user-plus" xlink:href="src/assets/symbol-defs.svg#icon-user-plus")
+					svg(class="icon-user-check")
+						use(class="icon-user-check" xlink:href="src/assets/symbol-defs.svg#icon-user-check")
+					| Opslaan
+</template>
+
+<script>
+import { itemBus } from '../main';
+
+export default {
+	props: ['items'],
+	data: function(){
+		return {
+			item: {
+				name: '',
+				jobTitle: '',
+				age: '',
+				bio: '',
+				selectedIcon: ''
+			},
+			icons: ['github','gitlab', 'user'],
+			isActive: null
+		}
+	},
+	methods: {
+		newItem() {
+			this.$emit('itemAdded', this.item);
+			this.item = '';
+			this.isActive = false;
+		},
+		focusFn(e) {
+			let el = e.target;
+			if( el.value == '' ) {
+				el.classList.toggle('input--filled');
+			}
+			el.addEventListener('blur', checkElement);
+
+			function checkElement() {
+
+				if( el.value.trim() !== '' ) {
+					el.classList.add( 'input--filled' );
+					checkField();
+				} else {
+					el.classList.remove('input--filled');
+				}
+			}
+
+			function checkField() {
+				document.getElementById('form-btn').removeAttribute('disabled');
+			}
+		},
+		closeOverlay() {
+			this.isActive = false;
+		}
+	},
+	created() {
+		itemBus.$on('overlayToggled', (isActive) => {
+			this.isActive = true;
+		});
+	}
+
+}
+</script>
